88ce489ea1cff7c7f66a238876894303a297db3c
Mr-man7352 <manissh7352@gmail.com>
2021-05-28 20:39:19 +0530

    add bottom to top button

--- js/covidSecion.js
+++ js/covidSecion.js
@@ -1,5 +1,4 @@
 // js for nav bar 
-
 $(window).scroll(function () {
     if ($(this).scrollTop() > 0 && $(window).width() > 992) {
       $(".navbar").addClass("nav-sticky");
@@ -7,4 +6,21 @@ $(window).scroll(function () {
       $(".navbar").addClass("nav-sticky");
     } else $(".navbar").removeClass("nav-sticky");
   });
+
+
+// js for bottom to top button
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $("#scroll").fadeIn();
+    } else {
+      $("#scroll").fadeOut();
+    }
+  });
+  $("#scroll").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 600);
+    return false;
+  });
+});
+
   
