9a6a372359b8ca0d80363155e92cb1d39f320264
Sachin Pandey <sachin@Sachins-MacBook-Air.local>
2021-05-28 18:21:52 +0530

    Second Page Added and Some Minor Media Queries and Js

---
+++ js/homepage.js
@@ -0,0 +1,8 @@
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 0 && $(window).width() > 992) {
+      $(".navbar").addClass("nav-sticky");
+    } else if ($(window).width() < 992) {
+      $(".navbar").addClass("nav-sticky");
+    } else $(".navbar").removeClass("nav-sticky");
+  });
+  
