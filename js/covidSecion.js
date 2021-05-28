a00c342fe7b41886dfdca73dbaf6e2460366a1bb
Mr-man7352 <manissh7352@gmail.com>
2021-05-28 17:54:54 +0530

    added css and js to navbar and added footer

---
+++ js/covidSecion.js
@@ -0,0 +1,10 @@
+// js for nav bar 
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 0 && $(window).width() > 992) {
+      $(".navbar").addClass("nav-sticky");
+    } else if ($(window).width() < 992) {
+      $(".navbar").addClass("nav-sticky");
+    } else $(".navbar").removeClass("nav-sticky");
+  });
+  
