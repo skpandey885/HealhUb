aab825ffcf98138cd4aa57b478fe7865cc251535
Atif0604 <atifcomputer2001@gmail.com>
2022-02-04 11:20:13 +0530

    Improved CSS

--- js/covidSecion.js
+++ js/covidSecion.js
@@ -53,45 +53,45 @@ fetch("https://corona.lmao.ninja/v2/countries/Australia,Chile,Germany,Hungary,In
         <th scope="row">1</th>
         <td data-label="Country">${result[0].country}</td>
         <td data-label="Cases">${result[0].cases}</td>
-        <td data-label="Critical Cases" class="newCases" >${result[0].critical}</td>
+        <td data-label="Critical Cases" class="newDeaths" >${result[0].critical}</td>
         <td data-label="Deaths">${result[0].deaths}</td>
-        <td data-label="Recovered" class="newDeaths" >${result[0].recovered}</td>
+        <td data-label="Recovered" class="newCases" >${result[0].recovered}</td>
         <td data-label="Active">${result[0].active}</td>
     </tr>
     <tr>
         <th scope="row">2</th>
         <td data-label="Country">${result[1].country}</td>
         <td data-label="Cases">${result[1].cases}</td>
-        <td data-label="Critical Cases" class="newCases" >${result[1].critical}</td>
+        <td data-label="Critical Cases" class="newDeaths" >${result[1].critical}</td>
         <td data-label="Deaths">${result[1].deaths}</td>
-        <td data-label="Recovered" class="newDeaths" >${result[1].recovered}</td>
+        <td data-label="Recovered" class="newCases" >${result[1].recovered}</td>
         <td data-label="Active">${result[1].active}</td>
     </tr>
     <tr>
         <th scope="row">3</th>
         <td data-label="Country">${result[2].country}</td>
         <td data-label="Cases">${result[2].cases}</td>
-        <td data-label="Critical Cases" class="newCases" >${result[2].critical}</td>
+        <td data-label="Critical Cases" class="newDeaths" >${result[2].critical}</td>
         <td data-label="Deaths">${result[2].deaths}</td>
-        <td data-label="Recovered" class="newDeaths" >${result[2].recovered}</td>
+        <td data-label="Recovered" class="newCases" >${result[2].recovered}</td>
         <td data-label="Active">${result[2].active}</td>
     </tr>
     <tr>
         <th scope="row">4</th>
         <td data-label="Country">${result[3].country}</td>
         <td data-label="Cases">${result[3].cases}</td>
-        <td data-label="Critical Cases" class="newCases" >${result[3].critical}</td>
+        <td data-label="Critical Cases" class="newDeaths" >${result[3].critical}</td>
         <td data-label="Deaths">${result[3].deaths}</td>
-        <td data-label="Recovered" class="newDeaths" >${result[3].recovered}</td>
+        <td data-label="Recovered" class="newCases" >${result[3].recovered}</td>
         <td data-label="Active">${result[3].active}</td>
     </tr>
     <tr>
         <th scope="row">5</th>
         <td data-label="Country">${result[4].country}</td>
         <td data-label="Cases">${result[4].cases}</td>
-        <td data-label="Critical Cases" class="newCases" >${result[4].critical}</td>
+        <td data-label="Critical Cases" class="newDeaths" >${result[4].critical}</td>
         <td data-label="Deaths">${result[4].deaths}</td>
-        <td data-label="Recovered" class="newDeaths" >${result[4].recovered}</td>
+        <td data-label="Recovered" class="newCases" >${result[4].recovered}</td>
         <td data-label="Active">${result[4].active}</td>
     </tr>
 </tbody>
