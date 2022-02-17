355e290930624faf493906a69554e6a0d9a3f7af
Pratyay Roy <59203815+DarkFalc0n@users.noreply.github.com>
2022-02-17 12:09:26 +0530

    added nearby hospital util

---
+++ js/hospitalmap.js
@@ -0,0 +1,60 @@
+
+const APIkey = "****API KEY HERE****";
+
+function fetchUrl(){
+  var url = `https://maps.googleapis.com/maps/api/js?key=${APIkey}&callback=initMap&libraries=places&v=weekly`;
+  return url;
+}
+
+var embed = document.getElementById("embedwrapper");
+
+
+var script = document.createElement('script');
+script.src = fetchUrl();
+embed.appendChild(script);
+
+
+function initMap() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((pos) => {
+        const uluru = { lat: pos.coords.latitude, lng: pos.coords.longitude };
+        const map = new google.maps.Map(document.getElementById("map"), {
+          zoom: 15,
+          center: uluru,
+
+
+        })   
+        var request = {
+          location: uluru,
+          radius: 1000,
+          types: ['hospital', 'health']
+        };
+        infowindow = new google.maps.InfoWindow();
+        var service = new google.maps.places.PlacesService(map);
+        service.nearbySearch(request, (res, status) => {
+          if (status == google.maps.places.PlacesServiceStatus.OK){
+            for (var i = 0; i < res.length; i++) {
+              var placeLoc = res[i].geometry.location;
+              var marker = new google.maps.Marker({
+                map: map,
+                position: placeLoc
+              })
+            }
+
+          }
+        }
+        )
+    }
+      )}
+
+    else {
+      console.log("Geolocation is not supported by this browser.");
+    }
+  }
+  
+
+
+    
+    
+
+
