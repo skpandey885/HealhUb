d751b6bd335c0c12bcfe8139650f8d4460762897
YatharthaRana <yathartharana2509@gmail.com>
2022-10-05 15:46:56 +0530

    Save the previous theme choice

--- js/homepage.js
+++ js/homepage.js
@@ -23,6 +23,23 @@ $(document).ready(function () {
 
 var darkswitch=document.getElementById("icon");
 
+if(localStorage.getItem("theme")==null)
+{
+  localStorage.setItem("theme","light");
+}
+
+let localData=localStorage.getItem("theme");
+if(localData=="light")
+{
+  darkswitch.src="assets/darkmoon.png";
+  document.body.classList.remove("dark-theme");
+}
+else if(localData=="dark")
+{
+  darkswitch.src="assets/sun.png";
+  document.body.classList.add("dark-theme");
+
+}
 
 darkswitch.onclick=function ()
 {
