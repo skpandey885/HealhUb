e03ee7db88138aa5fd0cdc5d13a5378b6c58e9cc
YatharthaRana <yathartharana2509@gmail.com>
2022-10-05 17:31:59 +0530

    Add dark theme on medicalHelp.html page

--- js/medicalHelp.js
+++ js/medicalHelp.js
@@ -44,7 +44,40 @@ $(document).ready(function () {
         })
       }
   };
-  
 
+var darkswitch=document.getElementById("icon");
+
+if(localStorage.getItem("theme")==null)
+{
+  localStorage.setItem("theme","light");
+}
+
+let localData=localStorage.getItem("theme");
+if(localData=="light")
+{
+  darkswitch.src="../assets/darkmoon.png";
+  document.body.classList.remove("dark_theme");
+}
+else if(localData=="dark")
+{
+  darkswitch.src="../assets/sun.png";
+  document.body.classList.add("dark_theme");
+
+}
+
+darkswitch.onclick=function ()
+{
+  document.body.classList.toggle("dark_theme");
+  if(document.body.classList.contains("dark_theme"))
+  {
+    darkswitch.src="../assets/sun.png";
+    localStorage.setItem("theme","dark");
+  }
+  else
+  {
+    darkswitch.src="../assets/darkmoon.png";
+    localStorage.setItem("theme","light");
+  }
+}
 
   
