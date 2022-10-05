6c90a8878b9df0fd5558467e3671400039943460
YatharthaRana <yathartharana2509@gmail.com>
2022-10-05 15:46:15 +0530

    Add dark mode toggle button

--- js/homepage.js
+++ js/homepage.js
@@ -20,3 +20,21 @@ $(document).ready(function () {
     return false;
   });
 });
+
+var darkswitch=document.getElementById("icon");
+
+
+darkswitch.onclick=function ()
+{
+  document.body.classList.toggle("dark-theme");
+  if(document.body.classList.contains("dark-theme"))
+  {
+    darkswitch.src="assets/sun.png";
+    localStorage.setItem("theme","dark");
+  }
+  else
+  {
+    darkswitch.src="assets/darkmoon.png";
+    localStorage.setItem("theme","light");
+  }
+}
