f712672b1d676107fc483d824811fcef7bd60e10
YatharthaRana <yathartharana2509@gmail.com>
2022-10-05 16:31:29 +0530

    Add dark theme in about.html page

--- js/about.js
+++ js/about.js
@@ -93,3 +93,37 @@ const scene3 = new ScrollMagic.Scene({
 function updatePercentage3() {
   t4.progress();
 }
+var darkswitch=document.getElementById("icon");
+
+if(localStorage.getItem("theme")==null)
+{
+  localStorage.setItem("theme","light");
+}
+
+let localData=localStorage.getItem("theme");
+if(localData=="light")
+{
+  darkswitch.src="assets/darkmoon.png";
+  document.body.classList.remove("dark-theme");
+}
+else if(localData=="dark")
+{
+  darkswitch.src="../assets/sun.png";
+  document.body.classList.add("dark-theme");
+
+}
+
+darkswitch.onclick=function ()
+{
+  document.body.classList.toggle("dark-theme");
+  if(document.body.classList.contains("dark-theme"))
+  {
+    darkswitch.src="../assets/sun.png";
+    localStorage.setItem("theme","dark");
+  }
+  else
+  {
+    darkswitch.src="../assets/darkmoon.png";
+    localStorage.setItem("theme","light");
+  }
+}
