d7aa1a74530fca211f06032243cf68b05fa48809
YatharthaRana <yathartharana2509@gmail.com>
2022-10-05 17:03:41 +0530

    Add dark theme on covidblogs.html page

--- js/covidBlogs.js
+++ js/covidBlogs.js
@@ -22,3 +22,38 @@ $(window).scroll(function () {
       $(".navbar").addClass("nav-sticky");
     } else $(".navbar").removeClass("nav-sticky");
   });
+
+var darkswitch=document.getElementById("icon");
+
+if(localStorage.getItem("theme")==null)
+{
+    localStorage.setItem("theme","light");
+}
+
+let localData=localStorage.getItem("theme");
+if(localData=="light")
+{
+    darkswitch.src="../assets/darkmoon.png";
+    document.body.classList.remove("dark-theme");
+}
+else if(localData=="dark")
+{
+    darkswitch.src="../assets/sun.png";
+    document.body.classList.add("dark-theme");
+
+}
+
+darkswitch.onclick=function ()
+{
+    document.body.classList.toggle("dark-theme");
+    if(document.body.classList.contains("dark-theme"))
+    {
+        darkswitch.src="../assets/sun.png";
+        localStorage.setItem("theme","dark");
+    }
+    else
+    {
+        darkswitch.src="../assets/darkmoon.png";
+        localStorage.setItem("theme","light");
+    }
+}
