233f782ff95b3570947aaf0ae61b65eac64babd3
YatharthaRana <yathartharana2509@gmail.com>
2022-10-06 15:11:04 +0530

    Replace toggle button for tips.html

--- js/tips.js
+++ js/tips.js
@@ -23,7 +23,7 @@ $(document).ready(function () {
     return false;
   });
 });
-
+//Darkmode
 var darkswitch=document.getElementById("icon");
 
 if(localStorage.getItem("theme")==null)
@@ -34,27 +34,29 @@ if(localStorage.getItem("theme")==null)
 let localData=localStorage.getItem("theme");
 if(localData=="light")
 {
-  darkswitch.src="../assets/darkmoon.png";
+  // darkswitch.src="../assets/darkmoon.png";
   document.body.classList.remove("dark-theme");
 }
 else if(localData=="dark")
 {
-  darkswitch.src="../assets/sun.png";
+  // darkswitch.src="../assets/sun.png";
+  darkswitch.classList.toggle("active");
   document.body.classList.add("dark-theme");
 
 }
 
 darkswitch.onclick=function ()
 {
+  darkswitch.classList.toggle("active");
   document.body.classList.toggle("dark-theme");
   if(document.body.classList.contains("dark-theme"))
   {
-    darkswitch.src="../assets/sun.png";
+    // darkswitch.src="../assets/sun.png";
     localStorage.setItem("theme","dark");
   }
   else
   {
-    darkswitch.src="../assets/darkmoon.png";
+    // darkswitch.src="../assets/darkmoon.png";
     localStorage.setItem("theme","light");
   }
 }
