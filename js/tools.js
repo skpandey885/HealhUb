55394526943e4e8e9afed3495f7c4f20bf77cb8e
YatharthaRana <yathartharana2509@gmail.com>
2022-10-06 15:11:20 +0530

    Replace toggle button for tools.html

--- js/tools.js
+++ js/tools.js
@@ -22,6 +22,8 @@ $(document).ready(function () {
     });
   });
 
+// darkmode js
+
 var darkswitch=document.getElementById("icon");
 
 if(localStorage.getItem("theme")==null)
@@ -32,27 +34,29 @@ if(localStorage.getItem("theme")==null)
 let localData=localStorage.getItem("theme");
 if(localData=="light")
 {
-    darkswitch.src="../assets/darkmoon.png";
+    // darkswitch.src="../assets/darkmoon.png";
     document.body.classList.remove("dark-theme");
 }
 else if(localData=="dark")
 {
-    darkswitch.src="../assets/sun.png";
+    // darkswitch.src="../assets/sun.png";
+    darkswitch.classList.toggle("active");
     document.body.classList.add("dark-theme");
 
 }
 
 darkswitch.onclick=function ()
 {
+    darkswitch.classList.toggle("active");
     document.body.classList.toggle("dark-theme");
     if(document.body.classList.contains("dark-theme"))
     {
-        darkswitch.src="../assets/sun.png";
+        // darkswitch.src="../assets/sun.png";
         localStorage.setItem("theme","dark");
     }
     else
     {
-        darkswitch.src="../assets/darkmoon.png";
+        // darkswitch.src="../assets/darkmoon.png";
         localStorage.setItem("theme","light");
     }
 }
